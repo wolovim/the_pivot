8050427e99d00afa0fe459205564b2e8f0f439d7
Allison Larson <allisonlarson91@gmail.com>
2014-08-13 14:50:48 -0600

    CRUD items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,48 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:edit, :update, :destroy]
+
   def index
     @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.save
+      redirect_to @item
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -9,4 +9,63 @@ RSpec.describe ItemsController, :type => :controller do
       expect(assigns(:items)).to eq([item])
     end
   end
+
+  describe "GET new" do
+    it 'assigns a new person as @person' do
+      get :new
+      expect(assigns(:item)).to be_a_new(Item)
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns the requested item as @item" do
+      item = Item.create valid_attributes
+      get :edit, {id: item.to_param}
+
+      expect(assigns(:item)).to eq(item)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid attributes" do
+      it "creates a new item" do
+        expect{
+          post :create, {:item => valid_attributes}
+        }.to change(Item, :count).by(1)
+      end
+
+      it 'assigns new item as @item' do
+        post :create, {:item => valid_attributes}
+        expect(assigns(:item)).to be_persisted
+      end
+    end
+
+    describe "with invalid params" do
+      it "re-renders the 'new' template" do
+        Item.any_instance.stub(:save).and_return(false)
+        post :create, {:item => {title: 'invalid'}}
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT update" do
+    describe "with valid attributes" do
+      it "updates the requested item" do
+        item = Item.create! valid_attributes
+        put :update, {:id => item.to_param, :item => valid_attributes}
+        expect(item).to eq(assigns(:item))
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested item" do
+      item = Item.create! valid_attributes
+      expect {
+        delete :destroy, {:id => item.to_param}
+      }.to change(Item, :count).by(-1)
+    end
+  end
+
 end
