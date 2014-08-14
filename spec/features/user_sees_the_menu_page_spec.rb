fe3f98637a25058658ef642d9f9c5236c5e6e271
ericfransen <ericfransen@Erics-MacBook-Pro.local>
2014-08-14 10:48:26 -0600

    Fix failing feature tests

--- spec/features/user_sees_the_menu_page_spec.rb
+++ spec/features/user_sees_the_menu_page_spec.rb
@@ -2,8 +2,10 @@ require_relative 'feature_spec_helper'
 
 describe 'the menu page', type: :feature do
   it 'shows a list of items' do
-    Item.create(title: 'food', description: 'delicious', price: 10.00)
-    Item.create(title: 'duck confils', description: 'expensive', price: 100.00)
+    item_1 = Item.create(title: 'food', description: 'delicious', price: 10.00)
+    item_2 = Item.create(title: 'duck confils', description: 'expensive', price: 100.00)
+    item_1.categories.create(name: "africa")
+    item_2.categories.create(name: "asia")
 
     visit '/menu'
     expect(page).to have_content('food')
@@ -11,15 +13,14 @@ describe 'the menu page', type: :feature do
   end
 
   it 'views items by category' do
-    item1 = Item.create(title: 'food', description: 'delicious', price: 10.00)
-    item2 = Item.create(title: 'more food', description: 'delicious', price: 10.00)
-    item1.categories.create(name: "africa")
-    item2.categories.create(name: "asia")
+    item_1 = Item.create(title: 'food', description: 'delicious', price: 10.00)
+    item_2 = Item.create(title: 'more food', description: 'delicious', price: 10.00)
+    item_1.categories.create(name: "africa")
+    item_2.categories.create(name: "asia")
 
     visit '/menu'
-    save_and_open_page
     page.click_on('africa')
-    expect(current_path).to eq(category_path(item1))
-    expect(page).to have_content(item1.title)
+    expect(current_path).to eq('/menu')
+    expect(page).to have_content(item_1.title)
   end
 end
