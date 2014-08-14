b8ace366ba294da991f2b3549b9b64f403e729f4
Andy Mention <amention@gmail.com>
2014-08-14 15:43:39 -0600

    Add menu link to carosel menu button

--- spec/features/user_can_interact_with_navbar.rb
+++ spec/features/user_can_interact_with_navbar.rb
@@ -2,8 +2,15 @@ require_relative 'feature_spec_helper'
 
 describe "the navbar", type: :feature do
   it "has buttons" do
-    pending
     visit "/"
     expect(page).to have_content("Home")
+  end
+
+  # should move out of navbar test because it tests the carosel
+  it "has a menu button" do
+    visit "/"
+    expect(page).to have_link("View Our Menu")
+    click_link("View Our Menu")
+    expect(current_url).to eq("http://www.example.com/menu")
   end
 end
