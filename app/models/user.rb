cc22dae92456df0aa4c89bd2e470609a6ebcdd09
Will Faurot <wfaurot@gmail.com>
2014-08-17 13:22:45 -0600

    Emails now case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :username,
             allow_blank: true,
             length: {
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,15 +25,20 @@ RSpec.describe User, :type => :model do
   end
 
   it 'is invalid without a unique email' do
-    user = User.create(first_name: 'Jane', 
-                       last_name: 'Doe', 
+    user = User.create(first_name: 'jane', 
+                       last_name: 'doe', 
                        email: 'email@example.com')
     expect(user).to be_valid
 
-    user2 = User.create(first_name: 'Jane', 
-                        last_name: 'Doe', 
+    user2 = User.create(first_name: 'jane', 
+                        last_name: 'doe', 
                         email: 'email@example.com')
     expect(user2).not_to be_valid
+
+    user3 = User.create(first_name: 'jane',
+                        last_name: 'doe',
+                        email: 'EMAiL@example.com')
+    expect(user3).not_to be_valid
   end
 
   it "is invalid if it's optional username betweeen 2 and 32 characters" do
