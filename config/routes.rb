6ac1359831c9292068417db3e1c0a4e8be663f88
Will Faurot <wfaurot@gmail.com>
2014-08-19 08:36:02 -0600

    Create admin dashboard and add links

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
       post :add_item
     end
   end
+
+  match '/admin_dashboard', to: 'admin#dashboard', via: :get
+
+  namespace :admin do
+    resources :items
+    resources :categories
+    resources :orders, only: [:index, :show, :edit, :update]
+  end
 end
--- spec/features/admin/admin_dashboard_view_spec.rb
+++ spec/features/admin/admin_dashboard_view_spec.rb
@@ -3,6 +3,10 @@ require_relative '../feature_spec_helper'
 describe 'the admin dashboard', type: :feature do
   it 'has links' do
     visit '/admin_dashboard'
-    expect(current_url).to eq("http://www.example.com/admin_dashboard")
+    
+    expect(page).to have_link("View Menu Items", href: admin_items_path)
+    expect(page).to have_link("View Orders", href: admin_orders_path)
+    expect(page).to have_link("Create New Item", href: new_admin_item_path)
+    expect(page).to have_link("Create New Category", href: new_admin_category_path)
   end
 end
