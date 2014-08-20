eb8d2b8f7e35e95561d7c4b427513fe405a3b9f4
Will Faurot <wfaurot@gmail.com>
2014-08-20 00:16:07 -0600

    Clean up tests

--- spec/features/admin/admin_items_view_spec.rb
+++ spec/features/admin/admin_items_view_spec.rb
@@ -2,6 +2,7 @@ require_relative '../feature_spec_helper'
 
 describe "admin items view", type: :feature do
   it 'has a list of menu items' do
+    # there's got to be a better way!  (gems)
     item1 = build_item(title: "Food", description: "Doe", price: 100.00)
     item2 = build_item(title: "MoreFood", description: "John", price: 150.00)
     item3 = build_item(title: "Foood", description: "Jane", price: 200.00)
@@ -13,17 +14,32 @@ describe "admin items view", type: :feature do
   end
 
   it 'has a link to view an individual item' do
-    item = build_item(title: "John", description: "Doe", price: 20.00)
+    item = build_item(valid_params)
 
     visit admin_items_path
-    expect(page).to have_link "View Item", admin_item_path(item)
+    expect(page).to have_link "View Item", href:admin_item_path(item)
   end
 
   it 'has a link to edit an item' do
-    item = build_item(title: "John", description: "Doe", price: 30.00)
+    item = build_item(valid_params)
 
     visit admin_items_path
-    expect(page).to have_link "Edit Item", edit_admin_item_path(item)
+    expect(page).to have_link "Edit Item", href:edit_admin_item_path(item)
+  end
+
+  it 'has a link to delete an item' do
+    item = build_item(valid_params)
+
+    visit admin_items_path
+    expect(page).to have_link "Delete Item", href:admin_item_path(item)
+  end
+
+  def valid_params
+    {
+      title: "John",
+      description: "Doe",
+      price: 20.00
+    }
   end
 
   def build_item(options={})
