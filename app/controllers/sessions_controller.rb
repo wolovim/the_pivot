239fcf9a2f3d72be0b31d08ee47d07e0d612194f
Alan Smith <a.bax.smith@gmail.com>
2014-08-20 10:34:16 -0600

    adds working login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'pages#home'
-
   resources :items
   resources :categories
-  get 'menu', to: 'items#index'
-
   resources :users, only: [:new, :create, :show, :index ]
   resources :sessions, only: [:new, :create, :destroy]
   resources :orders, except: [:new]
+
+  root 'pages#home'
+
+  get 'menu',       to: 'items#index'
+  match '/signup',  to: 'users#new',         via: 'get'
+  match '/login',  to: 'sessions#new',      via: 'get'
+  match '/logout', to: 'sessions#destroy',  via: 'delete'
+
 end
