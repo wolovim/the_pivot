e6dca7c6878eb02af316146df07180a7701cbceb
ericfransen <fransenep@gmail.com>
2014-08-21 15:20:10 -0600

    adds price to order button on nav bar, adds signed cookies to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
+    cookies.permanent.signed[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
     self.current_user = user
     order.update_attribute(:user_id, user.id)
@@ -17,7 +17,7 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.digest(cookies[:remember_token])
+    remember_token = User.digest(cookies.signed[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
