bd7fa0441f0b1e28a5c344974937790f1cb4de26
Alan Smith <a.bax.smith@gmail.com>
2014-08-21 09:41:36 -0600

    adds login tests, login form functional

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -1,7 +1,12 @@
 require_relative 'feature_spec_helper'
 
 describe "a user goes to the login page" do
-  user = User.new(first_name: "")
+  user = User.new
+  user.first_name = "John"
+  user.last_name  = "Doe"
+  user.email      = "j@example.com"
+  user.password   = "123456789"
+  user.save
 
   before do
     visit "/"
@@ -10,11 +15,19 @@ describe "a user goes to the login page" do
 
   describe "with valid parameters" do
     it "should login to their account" do
-      fill_in( "email address", :with => "a@example.com" )
+      fill_in( "email address", :with => "j@example.com" )
       fill_in( "password",      :with => "123456789" )
       click_button("Login")
-      save_and_open_page
+
       expect(page).to have_content("Your Cart")
+    end
+  end
+
+  describe "with invalid parameters" do
+    it "should render a new login template" do
+      click_button("Login")
+
+      expect(page).to have_content("Login.")
     end
   end
 end
