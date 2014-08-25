03ad2738b7c3d2ffe9b2e659b17a7fa3b26d8f6e
Will Faurot <wfaurot@gmail.com>
2014-08-25 16:50:21 -0600

    Fix merge stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824215244) do
+ActiveRecord::Schema.define(version: 20140825193917) do
 
   create_table "addresses", force: true do |t|
     t.integer  "order_id"
@@ -48,7 +48,11 @@ ActiveRecord::Schema.define(version: 20140824215244) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "price"
-    t.integer  "max_quantity", default: 500
+    t.integer  "max_quantity",       default: 500
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "order_items", force: true do |t|
