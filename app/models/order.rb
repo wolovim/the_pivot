e0ee686b2ea5f02c74f36ad0b793fc4fc5a4cbac
Andy Mention <amention@gmail.com>
2014-08-26 10:51:09 -0600

    Add item validation to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+  validates_associated :items
   has_many :addresses
   validates :delivery, inclusion: { in: [true, false] }
 
