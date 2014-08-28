08f5078116301834d97ab0347a3e3572a8c30a76
ericfransen <fransenep@gmail.com>
2014-08-28 09:50:25 -0600

    heroku

--- db/migrate/20140814170558_change_name_from_cart_to_order.rb
+++ db/migrate/20140814170558_change_name_from_cart_to_order.rb
@@ -4,6 +4,8 @@ class ChangeNameFromCartToOrder < ActiveRecord::Migration
     rename_table :cart_items, :order_items
     rename_column :order_items, :cart_id, :order_id
     rename_column :orders, :get_order, :delivery
-    change_column :orders, :delivery, :boolean
+    change_column :orders, :delivery, 'boolean USING CAST(delivery AS boolean)'
+
+
   end
 end
