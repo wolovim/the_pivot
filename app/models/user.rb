49a13ee717e17197c7aea8c081d8b1f79514384e
emilyadavis303 <emilyadavis303@gmail.com>
2014-09-10 17:18:55 -0600

    Removes username and adds city/state to registration form, fixes #1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,16 @@ class User < ActiveRecord::Base
   before_save { self.email == email.downcase }
   before_create :create_remember_token
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true, allow_blank: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :username,
-            allow_blank: true,
-            length: {
-              in: 2..32,
-              too_short: 'Username is too short.',
-              too_long: 'Username is too long.'
-            }
+  validates :first_name,  presence: true
+  validates :last_name,   presence: true, allow_blank: true
+  validates :email,       presence: true, uniqueness: { case_sensitive: false }
+  validates :password,    presence: true, length: { minimum: 8 }
+  validates :username,    allow_blank: true,
+                          length: {
+                            in: 2..32,
+                            too_short: 'Username is too short.',
+                            too_long: 'Username is too long.'
+                          }
 
   has_many :orders
 
