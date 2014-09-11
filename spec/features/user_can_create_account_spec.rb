b34adbcce1bae92483d993be27059cf6f8f89434
emilyadavis303 <emilyadavis303@gmail.com>
2014-09-11 11:03:10 -0600

    Fixes two broken user feature tests.

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -7,11 +7,11 @@ describe 'user', type: :feature do
     click_link("Signup")
     expect(page).to have_content("Join Today.")
 
-    fill_in( "first name",            :with => "Martha" )
-    fill_in( "last name",             :with => "Washington" )
-    fill_in( "email address",         :with => "mwashington@thepast.com" )
-    fill_in( "password",              :with => "password" )
-    fill_in( "password confirmation", :with => "password" )
+    fill_in( "First Name",            :with => "Martha" )
+    fill_in( "Last Name",             :with => "Washington" )
+    fill_in( "Email Address",         :with => "mwashington@thepast.com" )
+    fill_in( "Password",              :with => "password" )
+    fill_in( "Password Confirmation", :with => "password" )
     click_on("Create my account")
 
     expect(page).to have_content( "Welcome, Martha!")
