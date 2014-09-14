6db0037eaf17f1a720556fb842c57f6c3e0da653
Will Faurot <wfaurot@gmail.com>
2014-09-14 14:36:19 -0600

    Fix seed data (fixes #92)

--- spec/features/host_creates_a_new_listing_spec.rb
+++ spec/features/host_creates_a_new_listing_spec.rb
@@ -1,9 +1,6 @@
-require_relative '../feature_spec_helper'
+require_relative 'feature_spec_helper'
 
 describe 'host', type: :feature do
-  include AdminHelper
-
-  # before { log_me_in! }
 
   xit 'creates a first listing' do
     user = create :user, role: "default"
@@ -17,7 +14,6 @@ describe 'host', type: :feature do
     fill_in "Price", with: 20
     fill_in "People per unit", with: 1
     fill_in "Bathroom", with: "private"
-    # attach_file "Image", 'spec/fixtures/missing.jpg'
     click_button "Create Listing"
 
     expect(page).to have_content "NewListingTitle"
@@ -38,9 +34,8 @@ describe 'host', type: :feature do
     fill_in "Listing Title", with: "NewListingTitle"
     fill_in "Listing Description", with: "NewListingDescription"
     fill_in "Daily Rate", with: 20
-    # fill_in "People per unit", with: 1
-    # fill_in "Bathroom Type: Private or Shared", with: "private"
-    # attach_file "Image", 'spec/fixtures/missing.jpg'
+    # select "1", from: "People Per Unit"
+    # select "Shared", from: "Bathroom Type"
     click_button "Create Listing"
 
     expect(page).to have_content "NewListingTitle"
