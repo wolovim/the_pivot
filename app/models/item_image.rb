e12c74085ba345c7c476c7d625199a271b600220
Will Faurot <wfaurot@gmail.com>
2014-09-16 10:53:06 -0600

    Create nested images resource; fix typos

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,7 +1,7 @@
 class ItemImage < ActiveRecord::Base
   belongs_to :item
 
-  has_attached_file :item, :styles => { :small => "150x150>", :large => "320x240>" }
-  validates_attachment_presence :item
-  validates_attachment_size :item, :less_than => 5.megabytes
+  has_attached_file :image, :styles => { :small => "150x150>", :large => "320x240>" }
+  validates_attachment_presence :image
+  validates_attachment_size :image, :less_than => 5.megabytes
 end
