808176f08d390bd1435e43cb98dcebb6ffdc974c
Will Faurot <wfaurot@gmail.com>
2014-09-16 10:37:50 -0600

    Add associations for Item/ItemImages

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,7 @@
 class ItemImage < ActiveRecord::Base
   belongs_to :item
+
+  has_attached_file :item, :styles => { :small => "150x150>", :large => "320x240>" }
+  validates_attachment_presence :item
+  validates_attachment_size :item, :less_than => 5.megabytes
 end
