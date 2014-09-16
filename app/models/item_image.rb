76edf8137ce3211e9bdfa1641904c39df2b08882
Will Faurot <wfaurot@gmail.com>
2014-09-16 11:06:03 -0600

    Update paperclip stuffs in ItemImage

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,6 +2,6 @@ class ItemImage < ActiveRecord::Base
   belongs_to :item
 
   has_attached_file :image, :styles => { :small => "150x150>", :large => "320x240>" }
-  validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png','image/gif']
+  validates_attachment_size :image, :less_than => 1.megabytes
 end
