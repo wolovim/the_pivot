fa361b9409088d21b964f4ab1007bf14d95b0047
Will Faurot <wfaurot@gmail.com>
2014-09-17 10:27:49 -0600

    Add test for item_image

--- spec/models/item_image_spec.rb
+++ spec/models/item_image_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ItemImage, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with valid params' do
+    it 'has an image and an item' do
+      item = FactoryGirl.create :item
+      item_image = ItemImage.create(
+        image: File.open("spec/fixtures/missing.jpg", "r", item_id: item.id))
+
+      expect(item_image).to respond_to :image
+      expect(item_image).to respond_to :item
+    end
+  end
 end
