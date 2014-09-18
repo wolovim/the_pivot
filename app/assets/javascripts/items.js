b85301add4a84f023dfb37b50c0c362c0c6a10b6
ericfransen <fransenep@gmail.com>
2014-09-18 10:20:39 -0600

    saving changes to branch

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+
+  var deliveryAddress = $('#delivery-address');
+  var paymentInformation = $('#payment-info');
+
+  var list_of_items = [];
+  var filters = {};
+  function getItems(page) {
+    $.get('/items.json',
+      function(data, statusCode) {
+        if (statusCode == 'success') {
+          $.each(data, function(index, item) {
+            list_of_items.push(item);
+            renderItem( item );
+          });
+        } else {
+          console.log( statusCode, data );
+        }
+      }
+    );
+  }
+
+  $(".listings").html('');
+  getItems(1);
+
+  function renderItem(item) {
+    $(".listings").append('<div class="thumbnail col-lg-3">' +
+      '<img src="'+item.image_file_name+'">' +
+      '<div class="caption">' +
+        '<h5><a href="'+ item.path +'">'+item.title+'</a></h5>' +
+        '<p>$'+item.price+'</p>' +
+
+        // fix below, how not to pass authenticity token?
+        '<form action="/orders/25/add_item?item_id='+item.id+'" class="button_to" method="post">'+
+        '<div><input class="thumbnail-btn btn btn-primary" type="submit" value="Book it!">'+
+        '<input name="authenticity_token" type="hidden" value="ok/cMuVXVTaGgCMLmt58c4VIKP7BiANFFw6Hz75r3Pg="></div></form>' +
+      '</div>' +
+    '</div>');
+  }
+
+  function filterItems() {
+    $(".listings").html('');
+    $.each(list_of_items, function(index, item) {
+      var shouldBeVisible = true;
+      $.each(filters, function(key, value) {
+        // filters["price"] = [1000, 4000];
+        // item["price"] = 3000;
+        if (Array.isArray(value)) {
+          if (value[0] > item[key] || value[1] < item[key]) {
+            shouldBeVisible = false;
+          }
+        } else if (filters[key] !== item[key]) {
+          shouldBeVisible = false;
+        }
+      });
+      if (shouldBeVisible === true) {
+        renderItem( item );
+      }
+    });
+  }
+
+  $(".btn-group.bathrooms button").on("click", function(event) {
+    var filter_id = $(this).attr('id');
+    var was_selected = $(this).hasClass('active');
+    $(".btn-group.bathrooms button").removeClass('active');
+
+    if(was_selected) {
+      $(this).blur();
+      delete filters["bathroom"]
+    } else {
+      $(this).addClass('active');
+      filters["bathroom"] = filter_id;
+    }
+
+    filterItems();
+  });
+
+  $(".btn-group-vertical.accommodations button").on("click", function(event) {
+    var filter_id = $(this).attr('id');
+    var was_selected = $(this).hasClass('active');
+    $(".btn-group-vertical.accommodations button").removeClass('active');
+    $(this).addClass('active');
+    filters["accommodation"] = filter_id;
+    filterItems();
+  });
+  //
+  // $(".btn-group.people_per_unit button").on("click", function(event) {
+  //   var filter_id = $(this).attr('id');
+  //   $(".btn-group.people_per_unit button").removeClass('active');
+  //   $(this).addClass('active');
+  //   filters["people_per_unit"] = filter_id;
+  //   filterItems();
+  // });
+
+  $(".dropdown.people_per_unit li").on("click", function(event) {
+    var filter_id = $(this).attr('id');
+    filters["people_per_unit"] = filter_id;
+    filterItems();
+  });
+
+  // $(".btn-group.price-range").on("change", function() {
+  //   // Fix for price range
+  //   var min = $("#btn-range-min").value,
+  //       max = $("#btn-range-max").value;
+  //   filters["price"] = [min, max];
+  //   filterItems();
+  // });
+
+
+});
---
+++ spec/features/user_can_filter_listings_by_bathroom_spec.rb
@@ -0,0 +1,40 @@
+require_relative 'feature_spec_helper'
+
+describe 'user', type: :feature do
+  it 'gets json back' do
+
+    visit items_path(format: :json)
+    json_response = JSON.parse(page.body, symbolize_names: true)
+    expect(json_response).to have_content []
+  end
+
+  it 'gets back items' do
+    items = create_list :item, 3
+    visit items_path(format: :json)
+    json_response = JSON.parse(page.body, symbolize_names: true)
+    expect(json_response).to eq items.map { |item| JSON.parse(ItemSerializer.new(item, {root: false}).to_json, symbolize_names: true) }
+  end
+
+  xit 'can see bathroom filter options' do
+    visit items_path(format: :json)
+    # expect(page).to have_button 'Private'
+    expect(page).to have_css(".button", text: "Shared")
+  end
+
+  it 'can filter results by "private" bathroom' do
+    item1 = create :item
+    item1.title = "Private Bathroom Listing"
+    item1.bathroom = "Private"
+    item1.save
+
+    item2 = create :item
+    item2.title = "Shared Bathroom Listing"
+    item2.bathroom = "Shared"
+    item2.save
+
+    visit items_path
+    click_button "Private"
+    expect(page).to_not have_content('Shared Bathroom Listing')
+
+  end
+end
