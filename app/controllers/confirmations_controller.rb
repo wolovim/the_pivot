0d830196c77d5f6db2b35e181ae883e088e4de45
emilyadavis303 <emilyadavis303@gmail.com>
2014-09-22 10:01:51 -0600

    Fixes broken host spec.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,7 @@ class ConfirmationsController < ApplicationController
   def create
     listing_request.update_attributes!(confirmed: true)
     redirect_to listing_requests_path
+    confirmation_email
   end
 
   private
@@ -12,5 +13,17 @@ class ConfirmationsController < ApplicationController
 
   def listing_request
     listing_requests.find(params[:listing_request_id])
+  end
+
+  def confirmation_email
+    customer       = listing_request.order.user
+    customer_email = customer.email
+
+    Pony.mail(
+      :from    => "TravelHomeBookings@gmail.com",
+      :to      => "#{customer_email}",
+      :subject => "Your Booking Request has been Confirmed",
+      :body    => "Your request to book #{listing_request.item.title} has been confirmed."
+    )
   end
 end
