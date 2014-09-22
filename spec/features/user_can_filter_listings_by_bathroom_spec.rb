9f8fd8efc76f00cc75314c0b444b88928bf2960c
emilyadavis303 <emilyadavis303@gmail.com>
2014-09-21 18:03:54 -0600

    Makes a bit of progess on the user_can_filter_listings_by_bathroom_spec - it now clicks the 'private' method.

--- spec/features/user_can_filter_listings_by_bathroom_spec.rb
+++ spec/features/user_can_filter_listings_by_bathroom_spec.rb
@@ -2,7 +2,6 @@ require_relative 'feature_spec_helper'
 
 describe 'user', type: :feature do
   it 'gets json back' do
-
     visit items_path(format: :json)
     json_response = JSON.parse(page.body, symbolize_names: true)
     expect(json_response).to have_content []
@@ -22,19 +21,20 @@ describe 'user', type: :feature do
   end
 
   it 'can filter results by "private" bathroom' do
-    item1 = create :item
-    item1.title = "Private Bathroom Listing"
+    item1          = create :item
+    item1.title    = "Listing 1"
     item1.bathroom = "Private"
     item1.save
 
-    item2 = create :item
-    item2.title = "Shared Bathroom Listing"
+    item2          = create :item
+    item2.title    = "Listing 2"
     item2.bathroom = "Shared"
     item2.save
 
     visit items_path
-    click_button "Private"
-    expect(page).to_not have_content('Shared Bathroom Listing')
+    visit current_path
+    within(".btn-group.bathrooms") { click_button "Private" }
+    expect(page).to_not have_content('Listing 2')
 
   end
 end
