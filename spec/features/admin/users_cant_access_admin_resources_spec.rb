f7e7a0331acb9d283a89ed74ed816ffbac192b48
Will Faurot <wfaurot@gmail.com>
2014-09-23 17:21:09 -0600

    Add test for users accessing admin content

---
+++ spec/features/admin/users_cant_access_admin_resources_spec.rb
@@ -0,0 +1,28 @@
+require_relative "../feature_spec_helper"
+
+describe "non-admin", type: :feature do
+  let(:user) { FactoryGirl.create :user, role: "user" }
+
+  before do
+    visit "/login"
+
+    fill_in "email address", with: user.email
+    fill_in "password", with: user.password
+
+    click_button "Login"
+  end
+
+  it "can't access the admin dashboard" do
+    visit '/admin_dashboard'
+    expect(page).not_to have_link "Users"
+  end
+
+  it "can't acess the admin users page" do
+    visit '/admin/users'
+    expect(page).not_to have_content "Signup Date"
+  end
+
+  xit "can't access the admin listings page" do
+    visit '/admin/items'
+  end
+end
