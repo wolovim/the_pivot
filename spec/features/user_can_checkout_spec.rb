bb7b0d0b137495067c4620581e148c1a367836a4
emilyadavis303 <emilyadavis303@gmail.com>
2014-09-23 09:30:08 -0600

    Fixes spec tests failing on 'date'.

--- spec/features/user_can_checkout_spec.rb
+++ spec/features/user_can_checkout_spec.rb
@@ -22,7 +22,6 @@ describe 'A user who is logged in' do
     @order = create(:order, user_id: @user.id)
     @item  = create(:item,  user_id: @host.id)
     @user.orders << @order
-    @order.items << @item
 
     @order_item   = OrderItem.create(order_id: @order.id, item_id: @item.id)
     @availability = Availability.create(date: Time.now.to_date, order_item_id: @order_item.id)
@@ -39,28 +38,9 @@ describe 'A user who is logged in' do
     expect(page).to have_content("Confirm Booking Requests")
   end
 
-  xit 'can get to confirmation screen'  do
+  it 'can get to confirmation screen'  do
     click_on('Proceed to Booking Summary')
     click_on('Look Good? Send booking requests.')
     expect(page).to have_content('requested')
-  end
-
-  xit 'can add addresses' do
-    click_on('Proceed to Checkout')
-    fill_in 'address[street_1]', with: '123 Main St.'
-    fill_in 'address[city]', with: 'Denver'
-    fill_in 'address[state]', with: 'CO'
-    fill_in 'address[zip]', with: 123456
-    click_on('Save Address')
-    expect(page).to have_content('123 Main St.')
-  end
-
-  xit 'can add payment info' do
-    click_on('Proceed to Checkout')
-    fill_in 'order[ccn]', with: '1234567812345678'
-    fill_in 'order[expdate]', with: '12-12'
-    fill_in 'order[card_name]', with: 'Allison Larson'
-    click_on('Continue to Confirmation Screen')
-    expect(page).to have_content('Confirm Order')
   end
 end
