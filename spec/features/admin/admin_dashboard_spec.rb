ba7c72633611da0941f2d469aa6f1c9d80b564b7
Will Faurot <wfaurot@gmail.com>
2014-09-23 15:29:48 -0600

    Add test for redirecting admin to dashboard on login

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../feature_spec_helper'
 
-describe 'the admin dashboard', type: :feature do
-  include AdminHelper
+describe "admin", type: :feature do
 
-  before { login_as_admin }
-  
-  it 'has links' do
+  it "is redirected to the admin dashboard upon loggin in" do
+    user = FactoryGirl.create :user, role: "admin"
+
+    visit '/login'
+    fill_in "email address", with: user.email
+    fill_in "password", with: user.password
+    click_button "Login" 
+
+    expect(current_path).to eq admin_dashboard_path 
+  end
+
+  xit 'has the correct links' do
+    user = FactoryGirl.create :user, role: "admin"
+
+    visit '/login'
     visit '/admin_dashboard'
     expect(page).to have_link "Users", href: admin_users_path
     expect(page).to have_link "Listings", href: admin_orders_path
