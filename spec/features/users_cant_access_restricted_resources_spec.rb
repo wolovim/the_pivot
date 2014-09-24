40a03276eac1500e2a18b54aaf98242ef36cc3a0
Will Faurot <wfaurot@gmail.com>
2014-09-24 11:24:09 -0600

    Move test to more appropriate directory

---
+++ spec/features/users_cant_access_restricted_resources_spec.rb
@@ -0,0 +1,31 @@
+require_relative "feature_spec_helper"
+
+describe "non-admin", type: :feature do
+  let(:user) { FactoryGirl.create :user, role: "user" }
+
+  describe "logged-in user" do
+    before do
+      visit "/login"
+
+      fill_in "email address", with: user.email
+      fill_in "password", with: user.password
+
+      click_button "Login"
+    end
+
+    it "can't access the admin dashboard" do
+      visit '/admin_dashboard'
+      expect(page).not_to have_link "Users"
+    end
+
+    it "can't acess the admin users page" do
+      visit '/admin/users'
+      expect(page).not_to have_link "Delete User"
+    end
+
+    it "can't access the admin listings page" do
+      visit '/admin/items'
+      expect(page).not_to have_link "Delete Listing"
+    end
+  end
+end
