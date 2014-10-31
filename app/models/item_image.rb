92e700831fbf0a86676de3fe5aee289e2aa85871
Marc Garreau <marcdgarreau@gmail.com>
2014-10-31 16:31:21 -0600

    Cmonnn heroku

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,7 +1,7 @@
 class ItemImage < ActiveRecord::Base
   belongs_to :item
 
-  has_attached_file :image, :styles => { :small => "150x150#", :large => "300x300#" }
+  has_attached_file :image, :url => "https://s3.amazonaws.com/travehome/:basename.:extension", :styles => { :small => "150x150#", :large => "300x300#" }
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png','image/gif']
   validates_attachment_size :image, :less_than => 1.megabytes
 
