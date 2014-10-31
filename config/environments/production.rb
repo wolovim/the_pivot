7403239e72f21ac43f2ec6adb8542b3164e608d1
Marc Garreau <marcdgarreau@gmail.com>
2014-10-31 10:44:15 -0600

    Removes aws config stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Heroku S3 config
-  config.paperclip_defaults = {
-     :storage => :s3,
-     :s3_credentials => {
-       :bucket => ENV['AWS_BUCKET'],
-       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-     }
-  }
+#  config.paperclip_defaults = {
+#     :storage => :s3,
+#     :s3_credentials => {
+#       :bucket => ENV['AWS_BUCKET'],
+#       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+#     }
+#  }
 
 end
